e0d9bb13b8a1ba47da6eafba86642494be557330
ralfsumahe <269418914@qq.com>
2020-08-25 11:16:41 +0800

    路径修改

--- src/main/java/com/lk/pay/paypal/controller/PaypalController.java
+++ src/main/java/com/lk/pay/paypal/controller/PaypalController.java
@@ -17,7 +17,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/paypal")
+@RequestMapping("/pay/paypal")
 public class PaypalController {
     @Autowired
     private PayPalHttpClient payPalHttpClient;
