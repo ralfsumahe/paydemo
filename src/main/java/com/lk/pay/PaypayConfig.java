72a8fda16b8263567fb37b4f83f438612c4b1ddb
ralfsumahe <269418914@qq.com>
2020-08-26 16:36:51 +0800

    生产环境配置加入

--- src/main/java/com/lk/pay/PaypayConfig.java
+++ src/main/java/com/lk/pay/PaypayConfig.java
@@ -2,16 +2,27 @@ package com.lk.pay;
 
 import com.paypal.core.PayPalEnvironment;
 import com.paypal.core.PayPalHttpClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PaypayConfig {
+    @Value("${pay.paypal.client_id}")
+    private String clientId;
+    @Value("${pay.paypal.client_secret}")
+    private String clientSecret;
+    @Value("${pay.paypal.is_live}")
+    private Boolean isLive;
+
     @Bean
     PayPalEnvironment payPalEnvironment(){
-        String clientId = "ATN8f09gmB9Njm0iuoyApCV04GXbhbfltRzQPMHtjEL3i2oIZO0ShB31nIczY_hK1W0bVbYRUQaKIer6";
-        String clientSecret = "EGQzXV9F46OEBFBZHNkk04CAIXRwZW1r4K6X40anp2RKL4dYa4q-11qLxcuW1fyHZ-DA01Y9Oa_xC86j";
-        return new PayPalEnvironment.Sandbox(clientId,clientSecret);
+        if(isLive){
+            return new PayPalEnvironment.Live(clientId,clientSecret);
+        }else{
+            return new PayPalEnvironment.Sandbox(clientId,clientSecret);
+        }
+
     }
     @Bean
     PayPalHttpClient payPalHttpClient(){
